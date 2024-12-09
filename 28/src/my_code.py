efcd84c128dc076d81a7295621ceaf534ae640cc
Mario-Gozie <oguedoihucg@gmail.com>
2024-12-09 19:28:05 +0200

    more tasks in python

--- 28/src/my_code.py
+++ 28/src/my_code.py
@@ -93,6 +93,93 @@ from datetime import datetime
 #Write class and imports here!
 
 
+class Product:
+    def __init__(self, name="", price=0.0, location="", product_code=""):
+        self.name = name
+        self.price = price
+        self.location = location
+        self.product_code = product_code
+
+    def ask_data(self):
+        self.name = input("Name: ")
+        self.price = float(input("Price: "))
+        self.location = input("Location: ")
+        self.product_code = input("Code: ")
+
+    def __str__(self):
+        return (f"Name:       {self.name}\n"
+                f"Price:      {self.price:.2f}\n"
+                f"Location:   {self.location}\n"
+                f"Code:       {self.product_code}")
+
+
+class Cloth(Product):
+    def __init__(self, name="", price=0.0, location="", product_code="", size="", material=""):
+        super().__init__(name, price, location, product_code)
+        self.size = size
+        self.material = material
+
+    def ask_data(self):
+        super().ask_data()
+        self.size = input("Size: ")
+        self.material = input("Material: ")
+
+    def __str__(self):
+        return (super().__str__() + f"\nSize:       {self.size}\nMaterial:   {self.material}")
+
+
+class Grocery(Product):
+    def __init__(self, name="", price=0.0, location="", product_code="", country_of_origin="", best_before_date=""):
+        super().__init__(name, price, location, product_code)
+        self.country_of_origin = country_of_origin
+        self.best_before_date = best_before_date
+
+    def ask_data(self):
+        super().ask_data()
+        self.country_of_origin = input("Origin: ")
+        self.best_before_date = input("Best before: ")
+
+    def __str__(self):
+        return (super().__str__() + 
+                f"\nOrigin:     {self.country_of_origin}\nBest before:{self.best_before_date}")
+
+
+class Appliance(Product):
+    def __init__(self, name="", price=0.0, location="", product_code="", guarantee="", weight=""):
+        super().__init__(name, price, location, product_code)
+        self.guarantee = guarantee
+        self.weight = weight
+
+    def ask_data(self):
+        super().ask_data()
+        self.guarantee = input("Guarantee: ")
+        self.weight = input("Weight: ")
+
+    def __str__(self):
+        return (super().__str__() + f"\nGuarantee:  {self.guarantee}\nWeight:     {self.weight}")
+
+
+# Main program
+if __name__ == "__main__":
+    products = []
+
+    while True:
+        product_type = input("Type of product to add (1 = Groceries, 2 = Clothes, 3 = Appliance, other = quit): ").strip()
+        if product_type == "1":
+            product = Grocery()
+        elif product_type == "2":
+            product = Cloth()
+        elif product_type == "3":
+            product = Appliance()
+        else:
+            break
+
+        product.ask_data()
+        products.append(product)
+
+    print("\nProduct details:")
+    for product in products:
+        print("\n" + str(product))
 
 
 # if __name__ == "__main__":
